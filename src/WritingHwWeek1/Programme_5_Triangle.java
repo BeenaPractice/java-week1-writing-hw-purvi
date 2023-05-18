45b3fcb2c6d093ae443e452e2d9f2ff419783e11
Beenapractice <lalish1975@gmail.com>
2023-05-18 21:46:42 +0100

    Java week-1 homework submitted

---
+++ src/WritingHwWeek1/Programme_5_Triangle.java
@@ -0,0 +1,24 @@
+package WritingHwWeek1;
+/**
+ * 5.Write a Java program to print below triangle
+ *             *
+ *            * *
+ *           * * *
+ *          * * * *
+ *         * * * * *
+ *        * * * * * *
+ */
+public class Programme_5_Triangle {
+    public static void main (String[] args){
+        System.out.println("         *       ");
+        System.out.println("        * *      ");
+        System.out.println("       * * *     ");
+        System.out.println("      * * * *    ");
+        System.out.println("     * * * * *   ");
+        System.out.println("    * * * * * *  ");
+
+
+
+
+    }
+}
